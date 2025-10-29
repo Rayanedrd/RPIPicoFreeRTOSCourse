d67832e6236a0640cc96fed3bdab03e98c2962e0
Rayanedrd <rayanedridi7760@gmail.com>
2025-10-29 11:39:09 +0800

    Update main.cpp

--- 4-Tasks/MultiBlink/src/main.cpp
+++ 4-Tasks/MultiBlink/src/main.cpp
@@ -1,5 +1,5 @@
 /***
- * Demo program to flash an LED attached to GPIO PAD 0.
+ * Demo program to flash 5 LED attached to GPIO PAD 0, 2,3,4, 5.
  * Uses FreeRTOS Task
  * Jon Durrant
  * 15-Aug-2022
@@ -13,20 +13,21 @@
 #include <stdio.h>
 
 #include "BlinkAgent.h"
-#include "BlinkWorker.h"
-#include "BlinkHeavy.h"
 
 
 //Standard Task priority
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
 //LED PAD to use
-#define LED_PAD				6
-#define LED1_PAD			2
-#define LED2_PAD			3
-
-//DRIDI RAYANE
-//F14218807
+#define LED_PAD					1
+#define LED1_PAD				2
+#define LED2_PAD				3
+#define LED3_PAD				4
+#define LED4_PAD				5
+#define LED5_PAD				6
+#define LED6_PAD				7
+#define LED7_PAD				8
+#define LED8_PAD				9
 
 
 
@@ -71,6 +72,8 @@ void runTimeStats(   ){
    //Get heap allocation information
    HeapStats_t heapStats;
    vPortGetHeapStats(&heapStats);
+ 	printf("Dridi Rayane\n");
+	  printf("F14218807\n");
    printf("HEAP avl: %d, blocks %d, alloc: %d, free: %d\n",
 		   heapStats.xAvailableHeapSpaceInBytes,
 		   heapStats.xNumberOfFreeBlocks,
@@ -86,15 +89,26 @@ void runTimeStats(   ){
  */
 void mainTask(void *params){
 	BlinkAgent blink(LED_PAD);
-	BlinkWorker worker1(LED1_PAD);
-	BlinkHeavy worker2(LED2_PAD);
+	BlinkAgent worker1(LED1_PAD);
+	BlinkAgent worker2(LED2_PAD);
+	BlinkAgent worker3(LED3_PAD);
+	BlinkAgent worker4(LED4_PAD);
+	BlinkAgent worker5(LED5_PAD);
+	BlinkAgent worker6(LED6_PAD);
+	BlinkAgent worker7(LED7_PAD);
+	BlinkAgent worker8(LED8_PAD);
 
 	printf("Main task started\n");
 
 	blink.start("Blink", TASK_PRIORITY);
-	worker1.start("Worker 1", TASK_PRIORITY+ 1);
-	worker2.start("Worker 2", TASK_PRIORITY + 2);
-
+	worker1.start("Worker 1", TASK_PRIORITY);
+	worker2.start("Worker 2", TASK_PRIORITY + 1);
+	worker3.start("Worker 3", TASK_PRIORITY + 2);
+	worker4.start("Worker 4", TASK_PRIORITY + 3);
+	worker4.start("Worker 5", TASK_PRIORITY + 4);
+	worker4.start("Worker 6", TASK_PRIORITY + 5);
+	worker4.start("Worker 7", TASK_PRIORITY + 6);
+	worker4.start("Worker 8", TASK_PRIORITY + 7);
 	while (true) { // Loop forever
 		runTimeStats();
 		vTaskDelay(3000);
